ff7f592e9eb1c3b30d1925bd9405b7d0a409c2ad
daya <dayajuangmahaputra@gmail.com>
2022-11-28 20:36:16 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,20 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkNumber(12);
+    }
+
+    public static void checkNumber(int number){
+        String ans;
+        if(number == 0){
+            ans = "zero";
+            System.out.println(ans);
+            return;
+        }
+
+        ans = (number > 0) ? "positive" : "negative";
+
+        System.out.println(ans);
     }
 }
